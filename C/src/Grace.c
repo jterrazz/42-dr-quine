2ee9c49f1a82d0a1f2292233e457c69e980d0e02
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-05 15:07:00 +0200

    Grace C

---
+++ C/src/Grace.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define CODE "#include <stdio.h>%c#define CODE %c%s%c%c#define RET_SUCCESS return(0);%c#define FT()int main(void){ FILE *f = fopen(%cGrace_kid.c%c, %cw%c); if (f == NULL) return (1); fprintf(f, CODE, 10, 34, CODE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10);RET_SUCCESS}%c%c/*%c** 	Comment%c*/%c%cFT()%c"
+#define RET_SUCCESS return(0);
+#define FT()int main(void){ FILE *f = fopen("Grace_kid.c", "w"); if (f == NULL) return (1); fprintf(f, CODE, 10, 34, CODE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10);RET_SUCCESS}
+
+/*
+** 	Comment
+*/
+
+FT()
