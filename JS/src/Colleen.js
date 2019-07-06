6f6dd5c2740a7db1e42ea8ec0ddf87ba06bf0ba9
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-06 15:10:51 +0200

    Colleen JS

---
+++ JS/src/Colleen.js
@@ -0,0 +1,27 @@
+/*
+	Outside comment
+*/
+function printRecursion() {
+	const codeStr = `/*
+	Outside comment
+*/
+function printRecursion() {
+	const codeStr = %s%s%s
+	console.log(codeStr, String.fromCharCode(96), codeStr, String.fromCharCode(96))
+}
+function main() {
+	/*
+		Inside comment
+	*/
+	printRecursion()
+}
+main()`
+	console.log(codeStr, String.fromCharCode(96), codeStr, String.fromCharCode(96))
+}
+function main() {
+	/*
+		Inside comment
+	*/
+	printRecursion()
+}
+main()
