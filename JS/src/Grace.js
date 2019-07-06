e882bc6024222e328a159b75fcb75edcb23338d6
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-06 15:43:47 +0200

    Grace js

---
+++ JS/src/Grace.js
@@ -0,0 +1,14 @@
+(function grace() {
+	/*
+		This is a self invoking function, there are no defines in javascript
+	*/
+	const fs = require('fs');
+	const output = "Grace_kid.js"
+	const codeStr = `(${grace.toString()})();`
+
+	fs.writeFile(output, codeStr, function(err) {
+	    if (err) {
+	        return console.error(err);
+	    }
+	});
+})();
