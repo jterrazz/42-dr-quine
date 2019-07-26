7090515150974fbc02af89ac63e73d649954b69f
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-26 18:41:32 +0200

    D

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -4,7 +4,7 @@ global _main
 extern _printf
 
 section .rodata
-code_str db "; Outside comment%1$c%1$cglobal _main%1$cextern _printf%1$c%1$csection .rodata%1$ccode_str db %2$c%3$s%2$c%1$c%1$csection .text%1$c_main:%1$center 0, 0%1$clea rdi, [rel code_str] ; Inside comment%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel code_str]%1$ccall _printf%1$cleave%1$ccall return%1$c%1$creturn:%1$cret%1$c", 0
+code_str db "; Outside comment%1$c%1$cglobal _main%1$cextern _printf%1$c%1$csection .rodata%1$ccode_str db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c_main:%1$center 0, 0%1$clea rdi, [rel code_str] ; Inside comment%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel code_str]%1$ccall _printf%1$cleave%1$ccall return%1$c%1$creturn:%1$cret%1$c", 0
 
 section .text
 _main:
