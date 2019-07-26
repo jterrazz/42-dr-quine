7d3090c36a4a73b1752dbc8b2cc7425b9ded6a98
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-26 18:33:09 +0200

    Ft

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -6,7 +6,7 @@
 
 char *get_str()
 {
-	return "#include <stdio.h>%1$c%1$c/*%1$c** Hey, this is an outside comment%1$c*/%1$c%1$cchar *get_str()%1$c{%1$c%2$creturn %3$c%4$c%3$c;%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$c/*%1$c%2$c** Hey, this is an inside comment%1$c%2$c*/%1$c%2$cchar *str = get_str();%1$c%2$cprintf(str, 10, 9, 34, str);%1$c}%1$c";
+	return "#include <stdio.h>%1$c%1$c/*%1$c** Hey, this is an outside comment%1$c*/%1$c%1$cchar *get_str()%1$c{%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$c/*%1$c%2$c** Hey, this is an inside comment%1$c%2$c*/%1$c%2$cchar *str = get_str();%1$c%2$cprintf(str, 10, 9, 34, str);%1$c}%1$c";
 }
 
 int main(void)
